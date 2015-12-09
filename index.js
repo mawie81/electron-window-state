3a63878d8926a9fed1749c03845f11ae25f9d14d
Eran Tiktin <eran.t@mce-sys.com>
2015-12-10 00:35:45 +0200

    Update size and pos on resize and move
    Before this change, if you would close a maximized window, the size and
    position of it's unmaximize state won't be saved.
    This commit solves the issue, by listening to move and resize and
    updating that data.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const screen = electron.screen;
-const jsonfile = require('jsonfile');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const objectAssign = require('object-assign');
-const deepEqual = require('deep-equal');
+var app = require('app');
+var screen = require('screen');
+var jsonfile = require('jsonfile');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var objectAssign = require('object-assign');
+var deepEqual = require('deep-equal');
 
 module.exports = function (options) {
-  const config = objectAssign({}, {file: 'window-state.json', path: app.getPath('userData')}, options);
-  const fullStoreFileName = path.join(config.path, config.file);
-
-  let state;
+  var config = objectAssign({}, {
+    file: 'window-state.json',
+    path: app.getPath('userData')
+  }, options);
+  var fullStoreFileName = path.join(config.path, config.file);
+  var delay = 100; // Number of milliseconds to postpone handling the event
+  var state;
+  var winRef;
 
   try {
     state = jsonfile.readFileSync(fullStoreFileName);
@@ -24,7 +27,7 @@ module.exports = function (options) {
   if (state && state.displayBounds) {
     // If the display where the app window was displayed is no longer available,
     // we should drop the stored state
-    const displayBounds = screen.getDisplayMatching(state).bounds;
+    var displayBounds = screen.getDisplayMatching(state).bounds;
     if (!deepEqual(state.displayBounds, displayBounds, {strict: true})) {
       state = null;
     }
@@ -37,20 +40,79 @@ module.exports = function (options) {
     };
   }
 
+  var isNormal = function (win) {
+    return !win.isMaximized() && !win.isMinimized() && !win.isFullScreen();
+  }
+
   var saveState = function (win) {
-    const winBounds = win.getBounds();
-    const isMaximized = win.isMaximized();
-    if (!isMaximized && !win.isMinimized()) {
+    var winBounds = win.getBounds();
+    if (isNormal(win)) {
       state.x = winBounds.x;
       state.y = winBounds.y;
       state.width = winBounds.width;
       state.height = winBounds.height;
     }
-    state.isMaximized = isMaximized;
+    state.isMaximized = win.isMaximized();
     state.displayBounds = screen.getDisplayMatching(winBounds).bounds;
     mkdirp.sync(path.dirname(fullStoreFileName));
     jsonfile.writeFileSync(fullStoreFileName, state);
   };
+
+  var resizeTimer;
+  var resizeHandler = function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+      var winBounds = winRef.getBounds();
+      if (isNormal(winRef)) {
+        state.x = winBounds.x;
+        state.y = winBounds.y;
+        state.width = winBounds.width;
+        state.height = winBounds.height;
+      }
+    }, delay);
+  }
+
+  var moveTimer;
+  var moveHandler = function () {
+    clearTimeout(moveTimer);
+    moveTimer = setTimeout(function () {
+      var winBounds = winRef.getBounds();
+      if (isNormal(winRef)) {
+        state.x = winBounds.x;
+        state.y = winBounds.y;
+      }
+    }, delay);
+  }
+
+  var closeHandler = function () {
+    // Note: We wrap saveState because the first parameter of the close event is
+    // an event object and not the window object like it expects
+    saveState(winRef);
+  }
+
+  var closedHandler = function () {
+    unregister();
+  }
+
+  var register = function (win) {
+    win.on('resize', resizeHandler);
+    win.on('move', moveHandler);
+    win.on('close', closeHandler);
+    win.on('closed', closedHandler);
+    winRef = win;
+  }
+
+  var unregister = function () {
+    if (winRef) {
+      winRef.removeListener('resize', resizeHandler);
+      clearTimeout(resizeTimer);
+      winRef.removeListener('move', moveHandler);
+      clearTimeout(moveTimer);
+      winRef.removeListener('close', closeHandler);
+      winRef.removeListener('closed', closedHandler);
+      winRef = null;
+    }
+  }
 
   return {
     get x() { return state.x; },
@@ -58,6 +120,8 @@ module.exports = function (options) {
     get width() { return state.width; },
     get height() { return state.height; },
     get isMaximized() { return state.isMaximized; },
-    saveState: saveState
+    saveState: saveState,
+    register: register,
+    unregister: unregister
   };
 };
